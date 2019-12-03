48558ea2646ebafce32e99e0d4aa5ebe0a5a92ae
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-12-03 17:13:54 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,10 +1,13 @@
 # Your code goes here!
+require 'pry'
 class Anagram
+
   attr_accessor :word
   def initialize(word)
     @word=word
   end
   def match(match)
+    binding.pry
     match.collect do |x|
       x.
     end
