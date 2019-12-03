5f3db8574e47f11defb38eaee7b3740abb6e41eb
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-12-03 18:06:21 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,8 +7,8 @@ class Anagram
   end
   def match(match)
     binding.pry
-    match.map do |invividual|
-      new_match=individual.split(" ")
+    match.map do |match_words|
+      new_match=match_words.split(" ")
       if word_match.sort==new_match.sort
         @word[new_match]
       end
